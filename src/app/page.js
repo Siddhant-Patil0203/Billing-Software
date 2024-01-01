bc649cde11333785ce38cefe64f2e431c2c21165
Siddhant Patil <shimpli.patil2804@gmail.com>
2024-01-01 21:30:17 +0530

    Bill Header Added

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,26 @@
-import { Button } from "@nextui-org/button";
+import { Button, Code, Divider } from "@nextui-org/react";
 
 export default function Home() {
   return (
     <>
-      <div className="text-center text-3xl m-3">Home</div>
-      <Button className="m-3" color="primary" variant="shadow">Click Me</Button>
+      <div className="m-3 text-3xl font-semibold text-center text-red-600">
+        ASD Enterprises
+      </div>
+      <div className="m-3 text-center text-blue-600 font--medium text">
+        T-Shirts, Cup, Key Chain, Water Bottle, Photo Frame, Birthday Gifts with
+        Photo, Identity Cards Visiting Cards, Flex Banner, Glow Sign Board,
+        Trophies and all types of Digital Printing Works available
+      </div>
+      <div className="flex flex-col justify-between md:flex-row">
+        <Code className="m-3 text-center lg:p-4">GST No : 27AYPPD8713N1ZT</Code>
+        <Code color="danger" className="m-3">
+          <div className="flex flex-col items-center justify-center align-middle">
+            <div>Ashwin Deogade</div>
+            <div>(M) 8668527919 / 7249066711 </div>
+          </div>
+        </Code>
+      </div>
+      <Divider orientation="horizontal" />
     </>
   );
 }
