1ddf85f86038abcb845d307e6e10e9667f843dd7
Siddhant Patil <shimpli.patil2804@gmail.com>
2024-01-04 22:02:13 +0530

    Add bill details and item grid

--- src/app/page.js
+++ src/app/page.js
@@ -24,8 +24,163 @@ export default function Home() {
 
       {/* Main Grid */}
       <div className="grid grid-cols-12 gap-1 grid-rows-12">
+        {/* Heading/Title */}
         <div className="col-span-12 p-5 bg-pink-600"></div>
-        <div className="col-span-2 row-span-2 p-5 bg-purple-500"></div>
+
+        {/* Bill Details */}
+        <div className="col-span-3 p-5 bg-purple-500"></div>
+        <div className="col-span-3 p-5 bg-purple-500"></div>
+        <div className="col-span-3 p-5 bg-purple-500"></div>
+        <div className="col-span-3 p-5 bg-purple-500"></div>
+
+        {/* Deatils of Reciever */}
+        <div className="col-span-4 p-5 bg-pink-400"></div>
+        <div className="col-span-4 p-5 bg-pink-400"></div>
+        <div className="col-span-4 p-5 bg-pink-400"></div>
+
+        {/* <Divider className="w-full "/> */}
+
+        {/* Title of Items */}
+        <div className="col-span-1 p-5 bg-teal-300"></div>
+        <div className="col-span-3 p-5 bg-teal-300"></div>
+        <div className="col-span-1 p-5 bg-teal-300"></div>
+        <div className="col-span-1 p-5 bg-teal-300"></div>
+        <div className="col-span-1 p-5 bg-teal-300"></div>
+        <div className="col-span-2 p-1 bg-teal-300 ">
+          <div className="grid grid-rows-2 gap-1 grid-row-sub-2">
+            <div className="col-span-2 p-2 bg-teal-100"></div>
+            <div className="col-span-1 p-2 bg-teal-100"></div>
+            <div className="col-span-1 p-2 bg-teal-100"></div>
+          </div>
+        </div>
+        <div className="col-span-2 p-1 bg-teal-300">
+        <div className="grid grid-rows-2 gap-1 grid-row-sub-2">
+            <div className="col-span-2 p-2 bg-teal-100"></div>
+            <div className="col-span-1 p-2 bg-teal-100"></div>
+            <div className="col-span-1 p-2 bg-teal-100"></div>
+          </div>
+        </div>
+        <div className="col-span-1 p-5 bg-teal-300"></div>
+
+        {/* Items 1*/}
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-3 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>  
+
+        {/* Items 2*/}
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-3 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>  
+
+        {/* Items 3*/}
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-3 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>  
+
+        {/* Items 4*/}
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-3 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>  
+
+        {/* Items 5*/}
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-3 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>  
+
+        {/* Items 6*/}
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-3 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>  
+
+        {/* Items 7*/}
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-3 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>  
+        
+        {/* Items 8*/}
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-3 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>  
+
+        {/* Items 9*/}
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-3 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+
+        {/* Items 10*/}
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-3 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>
+        <div className="col-span-1 p-5 bg-yellow-300"></div>   
       </div>
     </>
   );
